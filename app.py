d3cebeb5fef15d872b4b93a8cab7a81ed2b3a8bb
Debarun Dhar <debarundhar@hotmail.com>
2017-11-17 07:51:22 -0500

    Add GAE deployment files

--- app.py
+++ app.py
@@ -2,18 +2,16 @@ from flask import Flask
 from mighty import settings
 from mighty.api import blueprint
 
+app = Flask(__name__)
 
-def initialize_app(flask_app):
-    flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
-    flask_app.config['RESTPLUS_VALIDATE'] = settings.RESTPLUS_VALIDATE
-    flask_app.config['RESTPLUS_MASK_SWAGGER'] = settings.RESTPLUS_MASK_SWAGGER
-    flask_app.config['ERROR_404_HELP'] = settings.RESTPLUS_ERROR_404_HELP
-    flask_app.register_blueprint(blueprint)
+app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
+app.config['RESTPLUS_VALIDATE'] = settings.RESTPLUS_VALIDATE
+app.config['RESTPLUS_MASK_SWAGGER'] = settings.RESTPLUS_MASK_SWAGGER
+app.config['ERROR_404_HELP'] = settings.RESTPLUS_ERROR_404_HELP
+app.register_blueprint(blueprint)
 
 def main():
-    app = Flask(__name__)
-    initialize_app(app)
-    app.run(debug=True)
+    app.run(port=8080, debug=True)
 
 if __name__ == "__main__":
     main()
